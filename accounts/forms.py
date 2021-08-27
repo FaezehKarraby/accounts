2c8f7438cc581e7fb62bc28fd8f6977dc0b26918
FaezehKarraby <38322228+FaezehKarraby@users.noreply.github.com>
2021-08-26 22:08:24 -0800

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,22 +4,6 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 
 
-'''class CreateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        exclude = ('user', )
-
-        def __init__(self, *args, **kwargs):
-            self.user = kwargs.pop('user')
-            super(CreateForm, self).__init__(*args, **kwargs)
-
-        def clean_title(self):
-            title = self.cleaned_data['title']
-            if Profile.objects.filter(user=self.user, title=title).exists():
-                raise forms.ValidationError('same title')
-            return title
-'''
-
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
